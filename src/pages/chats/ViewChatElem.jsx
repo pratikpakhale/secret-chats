28baa1f337cbe70f7103d4454dadf8278325c776
pratikpakhale <pratikpakhale20@gmail.com>
2022-04-17 19:43:21 +0530

    some ui fixes: bg color in view dms

--- src/pages/chats/ViewChatElem.jsx
+++ src/pages/chats/ViewChatElem.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function ViewChatElem({ name, chatID, message }) {
   return (
     <Link to={`/chat/${chatID}`}>
-      <div className='rounded-xl border-2 border-base-500 py-2 my-2 w-full lg:w-5/6 px-3 lg:px-10 md:px-5 mx-auto flex flex-col justify-center items-start cursor-pointer shadow-sm'>
+      <div className='rounded-xl border-2 border-base-500 py-2 my-2 w-full lg:w-5/6 px-3 lg:px-10 md:px-5 mx-auto flex flex-col justify-center items-start cursor-pointer shadow-sm bg-base-200'>
         <div className='font-poppins text-xl font-bold'>{name}</div>
         <div className='font-poppins text-lg'>{message}</div>
       </div>
